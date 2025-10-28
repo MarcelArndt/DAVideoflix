dc753d2ea040c4ce7b45e2180f3ffb83d80b1444
Marcel Arndt <marcel.arndt@hotmail.com>
2025-10-28 20:20:38 +0100

    adding documentation to service_app

--- service_app/apps.py
+++ service_app/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
-
+'''
+to register the signals.py
+ def ready(self):
+        import service_app.signals
+'''
 class ServiceAppConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "service_app"
--- service_app/models.py
+++ service_app/models.py
@@ -7,19 +7,6 @@ class Video(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     thumbnail_url = models.FileField(upload_to="uploads/thumbnails", blank=True)
     url = models.FileField(upload_to="uploads/videos/originals")
-    is_converted = models.BooleanField(default=False)
-    current_convert_state = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
 
     def __str__(self):
         return f'Id: {self.pk} | HeadLine: {self.title} |  Genre:{self.category} | created at: {self.created_at}' 
-
-
-class VideoProgress(models.Model):
-    updated_at = models.DateTimeField(auto_now=True)
-    video = models.ForeignKey(Video, on_delete=models.CASCADE)
-    current_time = models.FloatField()
-    is_finished = models.BooleanField(default=False)
-
-
-class CurrentVideoConvertProgress(models.Model):
-    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name='video_convert_progress')
