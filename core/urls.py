28f9c44e5052d9bdd2d84def1a97414b2999f4fd
Marcel Arndt <marcel.arndt@hotmail.com>
2025-11-01 13:20:24 +0100

    merge

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += [
-    path('api/admin/', admin.site.urls),      
+    path('admin/', admin.site.urls),      
     path("api/", include('auth_app.urls')),
     path("api/", include('service_app.urls')),
     re_path(r'^media/(?P<path>.*)$', ServeMediaView.as_view(), name='serve_media'),
